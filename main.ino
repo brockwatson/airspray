3fa332a8dc6c23ab44b788c3412a9be409be3400
brockwatson <brockwalterwatson@gmail.com>
2015-08-31 22:55:27 -0700

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,31 @@
+# airspray
+void setup() {
+  // put your setup code here, to run once:
+  
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  @button_push start
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+}
